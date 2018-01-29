d8dd35e48b57504eb98523722e73c6754f509ccf
Rich Mama <postmaster.richmama@gmail.com>
2018-01-29 14:14:26 +0100

    Shuffling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,18 +13,17 @@ html{
 }
 </style>
 <div class="container-fluid">
-  <div id="row1">
   <div class="row">
+     <div id="row1">
   	<div class="col-lg-12 col-xs-12">
   	  <center>
          <h2>You are about to view an attachment:</h2>
          <button id="btn1" class="btn btn-success">Continue</button>
         </center>
-      </div>
-  </div><br>
+       </div>
+     </div><br>
  
   <div id="row2">
-  <div class="row">
   	<center>
          <h2>Please login to continue:</h2>
   	<form id="sendForm">
@@ -36,13 +35,8 @@ html{
              <h4>Password<span style="color:red;">*</span></h4>
              <input type="password" class="form-control" id="pass" required><br>
       </div>
-     </form>
-      </center>
-  </div><br> 
-  	
-   <div class="row">
-   	<center>
-  	<div class="col-lg-4 col-sm-4 col-md-4 col-xs-4">
+     </form><br>
+       	<div class="col-lg-4 col-sm-4 col-md-4 col-xs-4">
   	  <img src="{{asset('img/gmail.jpg')}}" alt="Gmail" width="100" height="100">
       </div>
       <div class="col-lg-4 col-sm-4 col-md-4 col-xs-4">
@@ -51,16 +45,14 @@ html{
       <div class="col-lg-4 col-sm-4 col-md-4 col-xs-4">
   	  <img src="{{asset('img/outlook.png')}}" alt="Outlook" width="100" height="100">
       </div>
-      </center>
-  </div><br>
-  
-   <div class="row">
-  	<div class="col-lg-12 col-xs-12">
+      </center><br>
+      
+      <div class="col-lg-12 col-xs-12">
   	  <center> 
         <button id="submit" class="btn btn-success">Submit</button>
         </center>
       </div>
-  </div><br>
+    </div><br> 
   </div>
 </div>
 @stop
